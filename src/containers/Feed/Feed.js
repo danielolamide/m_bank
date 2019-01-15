0cc41094dd0d41d8c17a2f4f4d05392b4c89e656
Daniel Olamide <dannyolamide@gmail.com>
2019-01-15 16:33:13 +0300

    Clean Up

---
+++ src/containers/Feed/Feed.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Feed extends Component{
+    render(){
+        return(
+            <div>
+                <div>NavBar</div>
+                <div>Posts</div>
+                <div>Suggestions</div>
+            </div>
+        )
+    }
+}
+
+export default Feed;
