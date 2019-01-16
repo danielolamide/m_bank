a701af3d3e05b08b3ee50219ec25c212735c4bf5
Daniel Olamide <dannyolamide@gmail.com>
2019-01-16 14:52:03 +0300

    Mobile First Design Implemented

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
-import Feed from './containers/Feed/Feed';
+// import Feed from './containers/Feed/Feed';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-          <Feed/>
+        <div className ='top-nav'>Navigation</div>
+        <div className ='trends-wrapper'>Trends</div>
+        <div className='feed-wrapper'>Feed</div>
+        <div className='advertisment-wrapper'>Ads</div>
       </div>
     );
   }
